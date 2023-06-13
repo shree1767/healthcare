85419b4c11b388292e48f1b825ba0e30405b2224
shreenidhi <shreenidhi1757@gmail.com>
2023-06-13 15:16:50 +0530

    Sidebar Dashboard

---
+++ client/src/Components/SideDash/SideDash.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import './SideDash.css'
+import logout from './logout.svg'
+import patients from './patients.svg'
+import messages from './messages.svg'
+import settings from './settings.svg'
+import overview from './overview.svg'
+
+const SideDash = () => {
+  const links = [
+    { id: 1, title: 'Overview',image:overview},
+    { id: 2, title: 'Patients', image:patients},
+    { id: 3, title: 'Messages',image:messages },
+    { id: 4, title: 'Settings',image:settings},
+  ];
+
+  return (
+    <nav className="flex flex-col m-10 text-[30px] rounded-lg justify-between bg-[#006797] text-white h-[75vh] w-[20vw] pl-4 py-4">
+      <ul className="space-y-2 font-bold pt-5">
+        {links.map((link) => (
+          <li
+            key={link.id}
+            className="px-4 flex space-x-10 items-center py-2 rounded-[20px] hover:bg-white hover:text-[#006797] transition-colors cursor-pointer"
+          >
+            <img src={link.image} alt={link.title} className="w-6 h-6 mr-2" />
+            {link.title}
+          </li>
+        ))}
+      </ul>
+      <div className="p-4 mb-10 flex space-x-3 rounded-lg font-light  transition-colors cursor-pointer">
+        <img src={logout}/>
+        <span>Logout</span>
+      </div>
+    </nav>
+  );
+};
+
+export default SideDash;
+
