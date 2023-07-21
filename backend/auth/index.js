67ab35ce1ba8d5bd17b844d38ee9e352a38461cd
Pradyumna D <pradyumnad092004@gmail.com>
2023-07-21 22:16:00 +0530

    Patch

--- backend/auth/index.js
+++ backend/auth/index.js
@@ -1,11 +1,72 @@
 import express from "express";
 import { PrismaClient } from "@prisma/client";
 import cors from "cors";
+import generateResource from "../index.js";
+import fs, { readFile } from "fs";
+import * as path from "path";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 const port = 4000;
+
+let currentNumber = 99999;
+function getFormattedDate() {
+  const currentDate = new Date();
+
+  const year = currentDate.getFullYear();
+  const month = String(currentDate.getMonth() + 1).padStart(2, "0");
+  const day = String(currentDate.getDate()).padStart(2, "0");
+
+  return `${day}-${month}-${year}`;
+}
+
+function generateSequentialNumber() {
+  currentNumber = (currentNumber + 1) % 1000000;
+  return (currentNumber + 100000).toString();
+}
+
+async function readFiles(dir) {
+  try {
+    // read directory
+    let fileNames = await fs.promises.readdir(dir);
+
+    let data = [];
+
+    for (const filename of fileNames) {
+      const filepath = path.resolve(dir, filename);
+
+      // get information about the file
+      const stat = await fs.promises.stat(filepath);
+
+      // check if the current path is a file or a folder
+      const isFile = stat.isFile();
+      const ext = path.parse(filename).ext;
+
+      // exclude folders
+      if (isFile && ext === ".json") {
+        // callback, do something with the file
+        try {
+          const fileContent = await fs.promises.readFile(filepath, "utf-8");
+          data.push({
+            id: generateSequentialNumber(),
+            researchId:
+              JSON.parse(
+                fileContent
+              ).entry[0].resource.code.coding[0].code.toString(),
+            lastVisit: getFormattedDate(),
+            firstOccurrence: getFormattedDate(),
+          });
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    }
+    return data;
+  } catch (error) {
+    throw error;
+  }
+}
 
 app.post("/login", async (req, res) => {
   const { doctorID, password } = req.body;
@@ -74,6 +135,25 @@ app.post("/register", async (req, res) => {
   } catch (error) {
     console.log(error);
     return res.send(error);
+  }
+});
+
+app.get("/generate", async (req, res) => {
+  try {
+    generateResource();
+    res.send({ status: "success" });
+  } catch (error) {
+    console.log(error);
+    // res.send(error);
+  }
+});
+
+app.get("/getObservations", async (req, res) => {
+  try {
+    const data = await readFiles("../resources/observation");
+    res.send({ status: "success", data });
+  } catch (error) {
+    res.send(error);
   }
 });
 
--- backend/index.js
+++ backend/index.js
@@ -1,35 +1,37 @@
-const fs = require('fs');
-const {exec} = require("child_process")
-const newPatient = require("./newPatient")
-const newObservation = require("./newObservation.js")
+const fs = require("fs");
+const { exec } = require("child_process");
+const newPatient = require("./newPatient");
+const newObservation = require("./newObservation.js");
+
+// TODO: Convert the above im
 
 //Patient Resource maker functions
-const resourceMaker = (data) =>{
-
-  const arr = data.split(',')
-  const currID = arr[1]
-  exec(`ls ./resources/patient/${currID}*` , (out,o2,o3)=>{
-    if(o2.length > 0){
+const resourceMaker = (data) => {
+  const arr = data.split(",");
+  const currID = arr[1];
+  exec(`ls ../resources/patient/${currID}*`, (out, o2, o3) => {
+    if (o2.length > 0) {
       newObservation(arr);
-    }
-    else{
+    } else {
       newPatient(arr);
       newObservation(arr);
     }
-  })  
-}
+  });
+};
 
+const generateResource = () => {
+  // Read the CSV file
+  fs.readFile("data.csv", "utf8", (error, data) => {
+    if (error) {
+      console.error("Error reading the file:", error);
+      return;
+    }
+    // Split the CSV data into an array of rows
+    const rows = data.split("\n");
+    for (let i = 1; i < rows.length - 1; i++) {
+      resourceMaker(rows[i]);
+    }
+  });
+};
 
-// Read the CSV file
-fs.readFile('data.csv', 'utf8', (error, data) => {
-  if (error) {
-    console.error('Error reading the file:', error);
-    return;
-  }
-  // Split the CSV data into an array of rows
-  const rows = data.split('\n');
-  for(let i = 1 ; i < (rows.length)-1 ; i++){
-    resourceMaker(rows[i])
-  }
-
-});
+module.exports = generateResource;
