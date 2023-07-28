56f4bd3ddb16701e97d4aafd90033c3cfa561a08
Pradyumna D <pradyumnad092004@gmail.com>
2023-07-28 10:40:04 +0530

    Add dropdown for choose IVOM and remove patient list button

--- client/src/pages/Dashboard/PatientDetails.js
+++ client/src/pages/Dashboard/PatientDetails.js
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from "react";
-import { useLocation, Link, useNavigate } from "react-router-dom";
-import { Carousel } from "react-responsive-carousel";
+import React, {useEffect, useState} from "react";
+import {Link, useLocation, useNavigate} from "react-router-dom";
+import {Carousel} from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import Modal from "react-modal";
 import "./carousel.css";
@@ -9,99 +9,94 @@ import sec from "./assets/2.png";
 import heartimg from "./assets/heart.png";
 
 const PatientDetails = () => {
-  let navigate = useNavigate();
+    let navigate = useNavigate();
 
-  useEffect(() => {
-    const docData = JSON.parse(localStorage.getItem("docData"));
-    if (!docData) {
-      navigate("/");
-    }
-  }, []);
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  const openModal = (image) => {
-    setIsModalOpen(image);
-  };
-
-  const closeModal = () => {
-    setIsModalOpen(false);
-  };
-
-  const location = useLocation();
-  const patient = location.state;
-
-  return (
-    <div className="pt-5 px-5 h-screen overflow-y-auto">
-      <div className="flex flex-col md:flex-row space-y-3 md:space-y-0 md:space-x-10">
-        <Link
-          to="/patients"
-          className="bg-[#455B5D] mx-auto md:mx-0 w-1/2 md:w-[15vw] md:h-[6vh] py-[1vh] text-white md:text-[20px] font-[700] text-center rounded-lg"
-        >
-          Patient List
-        </Link>
-        <h1 className="md:text-[20px] text-[18px] text-[#194774] font-[700] py-[1vh]">
-          Patient Progression Visualisation and Modelling Dashboard
-        </h1>
-      </div>
-      <div className="rounded-lg bg-[#D6EDEF] py-8 md:px-8 px-2 my-2 grid md:grid-cols-4 gap-2 md:text-[20px]">
-        <div className="">
-          <p className="font-[700]">ID: {patient.id}</p>
-          <p>ResearchID: {patient.researchId}</p>
-          <p>Birth-Date: {patient.birthDate}</p>
-          <p>Eye: {patient.Eye}</p>
-        </div>
-        <div className="">
-          <p>Apoplex: {patient.Apoplex}</p>
-          <p>Arterielle Hypertonie: {patient.ArterielleHypertonie}</p>
-          <p>Blood Thinner:{patient.BloodThinner}</p>
-          <p>Diabetes Mellitus:{patient.DiabetesMellitus}</p>
-        </div>
-        <div className="">
-          <div className="flex space-x-5 bg-white rounded-lg items-center text-center w-[45vw] md:w-[23vw] px-5 py-3 mb-2">
-            <img src={heartimg} alt="heartimg" className="w-[8vw] h-[8vw] md:w-[5vw] md:h-[5vw]" />
-            <div>
-              <p>{patient.heartRate}</p>
-              <p className="text-[#008C16]">{patient.heartCondition}</p>
-            </div>
-          </div>
-          <p>IVOM Therapy:{patient.IVOMTherapy}</p>
-        </div>
-        {
-          <Link
-            to={`/prediction/${patient.id}`}
-            className="bg-[#455B5D] w-1/2 md:w-[15vw] h-[6vh] py-[1.5vh] text-white md:text-[20px] text-[15px] md:font-[700] font-[500] mx-auto text-center rounded-lg"
-          >
-            <button>Prediction Model</button>
-          </Link>
+    useEffect(() => {
+        const docData = JSON.parse(localStorage.getItem("docData"));
+        if (!docData) {
+            navigate("/");
         }
-      </div>
-      <div className="px-5">
-        <div className="">
-          <Carousel showArrows={true} className="custom-carousel">
-            <button onClick={() => openModal('first')}>
-              <img src={first} alt="first" className="max-w-[60vw] md:max-w-[25vw] cursor-pointer" />
-            </button>
-            <button onClick={() => openModal('second')}>
-              <img src={sec} alt="second" className="max-w-[60vw] md:max-w-[25vw] cursor-pointer" />
-            </button>
-          </Carousel>
-        </div>
+    }, []);
+    const [isModalOpen, setIsModalOpen] = useState(false);
 
-        <Modal
-          isOpen={isModalOpen}
-          onRequestClose={closeModal}
-          contentLabel="Image Modal"
-        >
-          <button onClick={closeModal} className="">X</button>
-          {isModalOpen === 'first' ? (
-            <img src={first} alt="Enlarged" className="max-w-full justify-center mx-auto" />
-          ) : (
-            <img src={sec} alt="Enlarged" className="max-w-full justify-center mx-auto" />
-          )}
-        </Modal>
-      </div>
-    </div>
-  );
+    const openModal = (image) => {
+        setIsModalOpen(image);
+    };
+
+    const closeModal = () => {
+        setIsModalOpen(false);
+    };
+
+    const location = useLocation();
+    const patient = location.state;
+
+    return (
+        <div className="pt-5 px-5 h-screen overflow-y-auto">
+            <div className="flex flex-col md:flex-row space-y-3 md:space-y-0 md:space-x-10">
+                <h1 className="md:text-[20px] text-[18px] text-[#194774] font-[700] py-[1vh]">
+                    Patient Progression Visualisation and Modelling Dashboard
+                </h1>
+            </div>
+            <div className="rounded-lg bg-[#D6EDEF] py-8 md:px-8 px-2 my-2 grid md:grid-cols-4 gap-2 md:text-[20px]">
+                <div className="">
+                    <p className="font-[700]">ID: {patient.id}</p>
+                    <p>ResearchID: {patient.researchId}</p>
+                    <p>Birth-Date: {patient.birthDate}</p>
+                    <p>Eye: {patient.Eye}</p>
+                </div>
+                <div className="">
+                    <p>Apoplex: {patient.Apoplex}</p>
+                    <p>Arterielle Hypertonie: {patient.ArterielleHypertonie}</p>
+                    <p>Blood Thinner:{patient.BloodThinner}</p>
+                    <p>Diabetes Mellitus:{patient.DiabetesMellitus}</p>
+                </div>
+                <div className="">
+                    <div
+                        className="flex space-x-5 bg-white rounded-lg items-center text-center w-[45vw] md:w-[23vw] px-5 py-3 mb-2">
+                        <img src={heartimg} alt="heartimg" className="w-[8vw] h-[8vw] md:w-[5vw] md:h-[5vw]"/>
+                        <div>
+                            <p>{patient.heartRate}</p>
+                            <p className="text-[#008C16]">{patient.heartCondition}</p>
+                        </div>
+                    </div>
+                    <p>IVOM Therapy:{patient.IVOMTherapy}</p>
+                </div>
+                {
+                    <Link
+                        to={`/prediction/${patient.id}`}
+                        className="bg-[#455B5D] w-1/2 md:w-[15vw] h-[6vh] py-[1.5vh] text-white md:text-[20px] text-[15px] md:font-[700] font-[500] mx-auto text-center rounded-lg"
+                    >
+                        <button>Prediction Model</button>
+                    </Link>
+                }
+            </div>
+            <div className="px-5">
+                <div className="">
+                    <Carousel showArrows={true} className="custom-carousel">
+                        <button onClick={() => openModal('first')}>
+                            <img src={first} alt="first" className="max-w-[60vw] md:max-w-[25vw] cursor-pointer"/>
+                        </button>
+                        <button onClick={() => openModal('second')}>
+                            <img src={sec} alt="second" className="max-w-[60vw] md:max-w-[25vw] cursor-pointer"/>
+                        </button>
+                    </Carousel>
+                </div>
+
+                <Modal
+                    isOpen={isModalOpen}
+                    onRequestClose={closeModal}
+                    contentLabel="Image Modal"
+                >
+                    <button onClick={closeModal} className="">X</button>
+                    {isModalOpen === 'first' ? (
+                        <img src={first} alt="Enlarged" className="max-w-full justify-center mx-auto"/>
+                    ) : (
+                        <img src={sec} alt="Enlarged" className="max-w-full justify-center mx-auto"/>
+                    )}
+                </Modal>
+            </div>
+        </div>
+    );
 };
 
 export default PatientDetails;
--- client/src/pages/Dashboard/Prediction.js
+++ client/src/pages/Dashboard/Prediction.js
@@ -1,60 +1,60 @@
 import React from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
+import {useLocation, useNavigate} from 'react-router-dom';
 import back from './assets/back.svg';
 import prediction from './assets/prediction.png';
 
 const Prediction = () => {
-  const location = useLocation();
-  const pathname = location.pathname;
-  const patientId = pathname.split('/').pop();
+    const location = useLocation();
+    const pathname = location.pathname;
+    const patientId = pathname.split('/').pop();
 
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  const navigateTo = () => {
-    navigate(`/patients/`);
-  };
+    const navigateTo = () => {
+        navigate(`/patients/`);
+    };
 
-  return (
-    <div className='md:pt-10 pt-20 px-5 h-screen overflow-y-auto'>
-      <div className='md:grid md:grid-cols-2 md:gap-5'>
-        <div>
-          <div className='flex space-x-3 items-center mb-4'>
-            <button onClick={navigateTo}>
-              <img src={back} alt='back' className='w-6 h-6' />
-            </button>
-            <h1 className='text-xl font-semibold'>Prediction Model</h1>
-          </div>
-          <div className='text-base'>
-            <p className='font-semibold'>Visual Acuity</p>
-            <p className='text-sm'>(Unit: Decimal)</p>
-          </div>
+    return (
+        <div className='md:pt-10 pt-20 px-5 h-screen overflow-y-auto'>
+            <div className='md:grid md:grid-cols-2 md:gap-5'>
+                <div>
+                    <div className='flex space-x-3 items-center mb-4'>
+                        <button onClick={navigateTo}>
+                            <img src={back} alt='back' className='w-6 h-6'/>
+                        </button>
+                        <h1 className='text-xl font-semibold'>Prediction Model</h1>
+                    </div>
+                    <div className='text-base'>
+                        <p className='font-semibold'>Visual Acuity</p>
+                        <p className='text-sm'>(Unit: Decimal)</p>
+                    </div>
+                </div>
+                <div className='text-base'>
+                    <div className='mb-4'>
+                        <label className='font-semibold'>Choose Date:</label>
+                        <input
+                            type='date'
+                            name='date'
+                            className='p-2 mx-2 mb-1 bg-white border border-[#006797] rounded'
+                            placeholder='Date'
+                        />
+                    </div>
+                    <div>
+                        <label className='font-semibold'>Choose IVOM Medication:</label>
+                        <select className="w-full px-4 py-2 rounded-lg border-2 border-black">
+                            <option value="Option 1">Option 1</option>
+                            <option value="Option 1">Option 2</option>
+                            <option value="Option 1">Option 3</option>
+                            <option value="Option 1">Option 4</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+            <div className='mt-4'>
+                <img src={prediction} alt='prediction' className='w-full md:w-[55vw] mx-auto'/>
+            </div>
         </div>
-        <div className='text-base'>
-          <div className='mb-4'>
-            <label className='font-semibold'>Choose Date:</label>
-            <input
-              type='date'
-              name='date'
-              className='p-2 mx-2 mb-1 bg-white border border-[#006797] rounded'
-              placeholder='Date'
-            />
-          </div>
-          <div>
-            <label className='font-semibold'>Choose IVOM Medication:</label>
-            <input
-              type='text'
-              name='text'
-              placeholder='Medication'
-              className='w-full text-[#006797] p-2 mb-1 border border-[#006797] rounded'
-            />
-          </div>
-        </div>
-      </div>
-      <div className='mt-4'>
-        <img src={prediction} alt='prediction' className='w-full md:w-[55vw] mx-auto' />
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Prediction;
